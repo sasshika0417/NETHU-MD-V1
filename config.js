a7f0c70e4a0910c5e175a2c1683c7a13ec3f989c
sasshika0417 <101021491+sasshika0417@users.noreply.github.com>
2024-10-07 13:57:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,14 +5,14 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put your session id ",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://imgtr.ee/images/2024/09/17/37f9be869d1404ef1a5d188178450537.jpeg",
-ALIVE_MSG: process.env.ALIVE_MSG || "Hello I'm alive now",
+SESSION_ID: process.env.SESSION_ID || "LdNFXLaJ#RpS5Em6zubd1aW2V2d5bOe5uxIHTvK-qhbwvazEKbzg ",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://ibb.co/R0CngQ9",
+ALIVE_MSG: process.env.ALIVE_MSG || "Hello I'm alive now Sabee",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
-SUDO_NB: process.env.SUDO_NB || "94704227534",
-OWNER_NAME: process.env.OWNER_NAME || "Nethmika MAIN",
-BOT_NAME: process.env.BOT_NAME || "NETHU-MD",
+SUDO_NB: process.env.SUDO_NB || "94758437638",
+OWNER_NAME: process.env.OWNER_NAME || "Sasshika Indusara",
+BOT_NAME: process.env.BOT_NAME || "Sabee-MD",
 AUTO_VOICE: process.env.AUTO_VOICE || "true"
 
 };
